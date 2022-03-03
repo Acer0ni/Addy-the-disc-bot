ea3cc1bc007920e5294bfee134a231620ceaf8fc
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-03-03 15:57:30 -0800

    refactored on_message event

--- bot.py
+++ bot.py
@@ -11,8 +11,49 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 jays_id = 322098392161452033
 seans_id = 310226357273493504
-client = discord.Client()
+COMMAND_CHARACTER = ["!","#"]
 count = 0
+
+
+
+async def command_99(message):
+    if message.author.id ==  jays_id:
+        await message.channel.send("lul nope")
+        return
+    response = random.choice(brooklyn_99_quotes)
+    await message.channel.send(response)
+
+async def command_count(message):
+    global count
+    await message.channel.send(f'{count}')
+    count += 1
+
+async def command_encourage(message):
+    with open("Data/encouragement.json") as f:
+        encouragement = json.load(f)
+    if message.author.id == seans_id:
+        await message.channel.send("sorry, nothing nice to say about Canadians")
+    response = random.choice(encouragement)
+    await message.channel.send(response)
+
+async def command_unknown(message):
+    await message.channel.send("sorry unknown command")
+
+command_map = {
+    '99':command_99,
+    'count':command_count,
+    'encourage':command_encourage
+}
+brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+client = discord.Client()
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -35,39 +76,14 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-    # elif message.author.id ==  jays_id:
-    #     print("jay is typing")
-    #     await message.channel.send("you suck jay")
-    #     return
-    print(message.author)
-    global count
-    with open("Data/encouragement.json") as f:
-        encouragement = json.load(f)
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    if message.content == '!99':
-        if message.author.id ==  jays_id:
-            await message.channel.send("lul nope")
-            return
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-    elif message.content == '!count':
-        await message.channel.send(f'{count}')
-        count += 1
-    elif message.content =='!encourage':
-        if message.author.id == seans_id:
-            await message.channel.send("sorry, nothing nice to say about Canadians")
-        response = random.choice(encouragement)
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
+    if message.content.startswith("!"):
+        command = message.content.split()[0][1:]
+        await command_map.get(command, command_unknown)(message)
+    if message.content == 'raise-exception':
         raise discord.DiscordException
+
+
+
 # class CustomClient(discord.Client):
 #     async def on_ready(self):
 #         print(f'{self.user} has connected to discord!')
