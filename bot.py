9d1bcb0a51aad1ddae197463261c6f2ca07bf4f2
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-03-04 15:55:53 -0800

    refactored with bots

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 import discord
 import json
 from dotenv import load_dotenv
+from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
@@ -13,52 +14,41 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 jays_id = 322098392161452033
 seans_id = 310226357273493504
-COMMAND_CHARACTER = ["!","#"]
 count = 1
+bot = commands.Bot(command_prefix='!')
 
-async def command_99(message):
+@bot.command(name='99')
+async def command_99(ctx):
     """
     Makes me tell you a random Brooklynn 99 quote
     """
-    if message.author.id ==  jays_id:
-        await message.channel.send("lul nope")
+    if ctx.author.id ==  jays_id:
+        await ctx.send("lul nope")
         return
     response = random.choice(brooklyn_99_quotes)
-    await message.channel.send(response)
+    await ctx.send(response)
 
-async def command_count(message):
+@bot.command(name='count')
+async def command_count(ctx):
     """
     Returns the amount of times this command has been used since last bot reset
     """
     global count
-    await message.channel.send(f'{count}')
+    await ctx.send(f'{count}')
     count += 1
 
-async def command_encourage(message):
+@bot.command(name='encourage')
+async def command_encourage(ctx):
     """
     responds with a word of encouragement
     """
     with open("Data/encouragement.json") as f:
         encouragement = json.load(f)
-    if message.author.id == seans_id:
-        await message.channel.send("sorry, nothing nice to say about Canadians")
+    if ctx.author.id == seans_id:
+        await ctx.send("sorry, nothing nice to say about Canadians")
     response = random.choice(encouragement)
-    await message.channel.send(response)
+    await ctx.send(response)
 
-async def command_help(message):
-    newline = "\n"
-    output = f'Available commands:{newline} !{f"{newline} !".join(list(command_map))}'
-    await message.channel.send(output)
-
-async def command_unknown(message):
-    await message.channel.send("sorry unknown command")
-
-command_map = {
-    '99':command_99,
-    'count':command_count,
-    'encourage':command_encourage,
-    'help':command_help
-}
 brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
         'Bingpot!',
@@ -67,41 +57,19 @@ brooklyn_99_quotes = [
             'no doubt no doubt no doubt no doubt.'
         ),
     ]
-client = discord.Client(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
     print(
-        f'{client.user} has connect to Discord:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{bot.user.name} has connect to Discord:\n'
+
         )
+#need to fix this
+# @client.event
+# async def on_member_join(member):
+#     this_guild = member.guild
+#     channel = this_guild.text_channels[0]
+#     await channel.send(f"Hi {member.name}, welcome to my Discord server!")
 
-@client.event
-async def on_member_join(member):
-    print("someone joined!")
-    # await member.create_dm()
-    # await member.dm_channel.send(
-    #     f"Hi {member.name}, welcome to my Discord server!"
-    # )
-    this_guild = member.guild
-    print(this_guild.id)
-    channel = this_guild.text_channels[0]
-    print(channel)
-    await channel.send(f"Hi {member.name}, welcome to my Discord server!")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("!"):
-        command = message.content.split()[0][1:]
-        await command_map.get(command, command_unknown)(message)
-    if message.content == 'raise-exception':
-        raise discord.DiscordException
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
 
