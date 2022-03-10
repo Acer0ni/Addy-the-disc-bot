2bfda472ae7d582776be0c1f30620ff116ac03c1
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-03-10 14:29:31 -0800

    added activites to highscores command

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from cmath import exp
 import os
 import random
+from unicodedata import category
 import discord
 import json
 import requests
@@ -12,6 +13,7 @@ intents.presences = True
 
 load_dotenv()
 skill = ['Overall','Attack', 'Defence', 'Strength', 'Constitution', 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction', 'Summoning', 'Dungeoneering', 'Divination', 'Invention', 'Archaeology']
+activities = ['Bounty Hunter','B.H. Rogues','Dominion Tower','The Crucible','Castle Wars games', 'B.A. Attackers', 'B.A. Defenders', 'B.A. Collectors','B.A. Healers', 'Duel Tournament','Mobilising Armies','Conquest','Fist of Guthix','GG: Athletics','GG: Resource Race','WE2: Armadyl Lifetime Contribution','WE2: Bandos Lifetime Contribution','WE2: Armadyl PvP kills','WE2: Bandos PvP kills','Heist Guard Level','Heist Robber Level','CFP: 5 game average','AF15: Cow Tipping','AF15: Rats killed after the miniquest','RuneScore', 'Clue Scrolls Easy','Clue Scrolls Medium','Clue Scrolls Hard','Clue Scrolls Elite','Clue Scrolls Master']
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 jays_id = 322098392161452033
@@ -20,7 +22,7 @@ count = 1
 bot = commands.Bot(command_prefix='!',intents=intents)
 
 #deal with multi word names
-@bot.command(name='hs')
+@bot.command(name='hs',category ='runescape')
 async def command_HS(ctx,*args):
     """
     use this command to look up a player in RS3
@@ -35,12 +37,19 @@ async def command_HS(ctx,*args):
         return
     formated_response =highscores_formatter(response.content)
     new_line= '\n'
-    new_string = ""
-    for x in range(29):
-        exp=int(formated_response[x][2])
-        rank=int(formated_response[x][0])
-        new_string += f"{skill[x]} level: {formated_response[x][1]} experience: {exp:,} Rank: {rank:,} {new_line}"
-    await ctx.send(new_string)
+    skill_string = f"**{username}** {new_line}"
+    activity_string =""
+    for x in range((len(formated_response))):
+        if x < (len(skill) -1):
+            exp=int(formated_response[x][2])
+            rank=int(formated_response[x][0])
+            skill_string += f"**{skill[x]}** level: {formated_response[x][1]} experience: {exp:,} Rank: {rank:,} {new_line}"
+        elif x > (len(skill) - 1):
+            rank = int(formated_response[x][0])
+            score = int(formated_response[x][1])
+            activity_string += f"**{activities[x - (len(skill))]}** Rank: {rank:,} Score: {score:,} {new_line}"
+    await ctx.send(skill_string)
+    await ctx.send(activity_string)
 
 @bot.command(name='99')
 async def command_99(ctx):
