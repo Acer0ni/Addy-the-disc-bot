4c4bc25f34b50a41148dbf9c028eb721993106ff
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-03-29 13:58:04 -0700

    deployment fix's

--- addy/db.py
+++ addy/db.py
@@ -5,5 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DB_CREDENTIALS = os.getenv("DB_CREDENTIALS")
-engine = sqlalchemy.create_engine(f"postgresql://{DB_CREDENTIALS}@localhost/addy")
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+engine = sqlalchemy.create_engine(f"postgresql://{DB_CREDENTIALS}@{DB_HOSTNAME}/addy")
 Session = sessionmaker(engine)
