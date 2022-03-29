a40fae164fac41d57c791f4e68436e1e79194fa7
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-03-29 11:58:39 -0700

    added __repr__ and str functions

--- addy/models/coin.py
+++ addy/models/coin.py
@@ -10,3 +10,9 @@ class Coin(Base):
     name = Column(String(255), unique=False, nullable=True)
     symbol = Column(String(255), unique=False, nullable=True)
     coingecko_id = Column(String(255), unique=False, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"<Coin name={self.name} symbol={self.symbol}>"
+
+    def __str__(self) -> str:
+        return self.name
