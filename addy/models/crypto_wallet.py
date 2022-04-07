b9ff78dab4b7c2dac89f9905f489d83f0704997e
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-04-06 19:29:37 -0700

    bug fix

--- addy/models/crypto_wallet.py
+++ addy/models/crypto_wallet.py
@@ -9,12 +9,13 @@ class Crypto_wallet(Base):
     __tablename__ = "crypto_wallet"
 
     id = Column(Integer, primary_key=True, autoincrement="auto")
+    # balance needs to be a float
     balance = Column(
         Float,
         unique=False,
         nullable=False,
         default=10_000,
-        server_default="10000",
+        server_default=text("10000"),
     )
     transactions = relationship("Transaction", back_populates="wallet")
     crypto_holdings = relationship("Crypto_holding", back_populates="crypto_wallet")
